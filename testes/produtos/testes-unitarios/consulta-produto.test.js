ac9f39951f2ba6a393259cff41be4ccdceadabc8
TaynaraLuanaQA <ctaynaraluana@gmail.com>
2025-01-04 12:37:55 -0300

    estrutura-projeto

---
+++ testes/produtos/testes-unitarios/consulta-produto.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const urlApi = "http://localhost:3000";
+
+describe('Suite de testes de api - para validar consulta de produtos. (FLUXO SUCESSO)', ()=>{
+    it('CE01: Consultando todos os produtos cadastrados com sucesso. É esperado retornar 200', async()=>{
+        const resposta = await request(urlApi).get('/products');
+        expect(resposta.statusCode).toBe(200);
+        console.log('O status da requisição retornou: ',resposta.statusCode)
+    });
+
+    it('CE02: Consultando todos os produtos cadastrados com sucesso. Deve validar que foi retornado um array de objetos.', async()=>{
+        const resposta = await request(urlApi).get('/products');
+        expect(resposta.statusCode).toBe(200);
+        //verifica se é um array de objetos, (true) se é verdadeiro.
+        expect(Array.isArray(resposta.body)).toBe(true);
+
+        console.log('O status da requisição retornou: ',resposta.statusCode);
+        console.log('A resposta da requisição retornou o seguinte body: ', resposta.body);
+    });
+})
